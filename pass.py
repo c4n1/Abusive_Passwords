4c157c29ab91d2aba464ceec80411c4458317208
Oliver Hills <oli@deathbycomputers.co.uk>
2017-01-30 15:11:36 +0800

    dumb things

--- pass.py
+++ pass.py
@@ -2,10 +2,11 @@
 import random
 
 Words = {}
+Seps = ['_','-','^','#','~','+','=']
 with open('Words/working', 'r') as f:
     for line in f:
         Words = [line.strip() for line in f]
 
 
-out = (random.choice(Words),'_',random.choice(Words),'_',random.choice(Words))
+out = (random.choice(Words),random.choice(Seps),random.choice(Words),random.choice(Seps),random.choice(Words))
 print ''.join(map(str, out))
