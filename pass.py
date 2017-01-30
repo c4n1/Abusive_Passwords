fc5e3c1ada5263ac2a4cc81893c560734e1aacb3
Oliver Hills <oli@deathbycomputers.co.uk>
2017-01-30 15:00:58 +0800

    I did a silly thing

---
+++ pass.py
@@ -0,0 +1,14 @@
+#!/bin/python
+
+from pprint import pprint
+import random
+
+Words = {}
+with open('Words/working', 'r') as f:
+    for line in f:
+        Words = [line.strip() for line in f]
+
+
+out = (random.choice(Words),'_',random.choice(Words),'_',random.choice(Words))
+
+print ''.join(map(str, out))
